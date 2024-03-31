4ef39e19c9f1127eff06807477dc34346e8944d7
Sunboong-Kim <kikiboong@dongguk.edu>
2024-03-31 22:20:35 +0900

    2024-03-31 개인과제5

---
+++ Psub5/Student.java
@@ -0,0 +1,23 @@
+package Psub5;
+import java.util.Scanner;
+
+public class Student {
+
+	public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("이름을 입력하세요 :");
+        String name = sc.nextLine();
+
+        System.out.print("학번을 입력하세요 :");
+        String id = sc.nextLine();
+
+        System.out.println("<출력>");
+        System.out.println("이름: " + name);
+        System.out.println("학번: " + id);
+
+        sc.close();
+
+    }
+}
